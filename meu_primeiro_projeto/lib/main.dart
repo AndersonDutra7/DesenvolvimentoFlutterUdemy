925bfba6c45b430c8498f231dd1c7ac505f18993
Anderson Dutra <andersonrdutra7@gmail.com>
2025-04-24 22:26:00 -0300

    Criado primeir projeto

---
+++ meu_primeiro_projeto/lib/main.dart
@@ -0,0 +1,74 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meu Primeiro Projeto Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+      ),
+      home: const MyHomePage(title: 'Frases Sortidas'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _numeroAleatorio = 0;
+  final List _frases = [
+    "Bem vindo ao Flutter",
+    "Seja persistente, você vai conseguir",
+    "Foco nos estudos",
+    "Tenha constância",
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+      _numeroAleatorio = Random().nextInt(4);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Pressione o botão para gerar frases'),
+            Text(
+              _frases[_numeroAleatorio],
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
