e046728445d53a97a701393ad45e28de994ce6a3
Anderson Dutra <andersonrdutra7@gmail.com>
2025-05-10 23:25:56 -0300

    Ajustado aulas iniciais Widget+ Material Design

---
+++ aula_componentes_interface/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:aula_componentes_interface/aula_widgets.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MeuApp(), debugShowCheckedModeBanner: false));
+}
